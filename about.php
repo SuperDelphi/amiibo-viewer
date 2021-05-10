3cebed276433269d681d656253154a5a2b6395b7
Simon Breil <simon.breil@protonmail.com>
2021-05-10 11:43:36 +0200

    Création de about.php

---
+++ about.php
@@ -0,0 +1,51 @@
+<!--
+  ~ Copyright (c) Simon Breil 2021
+  -->
+
+<!--
+  ~ Copyright (c) Simon Breil 2021
+  -->
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Amiibo Viewer | À propos</title>
+        <link rel="shortcut icon" href="favicon.ico">
+        <link rel="stylesheet" href="css/about/main.css">
+    </head>
+    <body>
+
+        <?php include_once "includes/header.php"; ?>
+
+        <main>
+            <h1>À propos</h1>
+            <p>
+                Ce site web a été réalisé sur une durée de 5 jours. Bien que sa réalisation soit issue d'un projet universitaire qui doit conclure un semestre,
+                il s'agissait avant tout de mettre en pratique mes compétences dans une création concrète.
+            </p>
+            <p>
+                Je me suis aussi imposé un court temps pour le développer, non pas pour épargner mon temps libre mais
+                pour mettre ma productivité à l'épreuve, sans pour autant empiéter sur la qualité finale du site.
+            </p>
+            <p>
+                Ce site contient de nombreuses fonctionnalités techniques intéressantes, telles qu'un système de pagination,
+                de défilement infini ou encore un système de cache rudimentaire.
+            </p>
+            <p>
+                Pour terminer, si quelques spécificités du produit vous intéressent,
+                n'hésitez pas à parcourir le code du site à l'aide des outils de développeur de votre navigateur
+                et à vous inspirer de sa structure. Celle-ci est loin d'être parfaite, mais elle a eu l'opportunité
+                d'être refactorée à de multiples reprises, afin d'améliorer la qualité de la "codebase".
+            </p>
+            <hr>
+            <p class="signature">Simon Breil</p>
+        </main>
+
+        <?php include_once "includes/hamburger.php"; ?>
+
+    </body>
+</html>
